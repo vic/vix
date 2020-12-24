939f5f30186079362b4803198cd218de14fbf95e
Victor Borja <vborja@apache.org>
2020-12-24 14:32:41 -0600

    nixfmt

--- nix/vic.nix
+++ nix/vic.nix
@@ -2,29 +2,38 @@
 
 {
   home-manager.users.vic = {
-            home.packages = with pkgs; [
-              bat               # better cat
-              browsh            # Firefox on shell
-              coursier          # scala dependencies
-              dbmate            # migrations
-              direnv            # environment per project
-              emacsMacport      # one editor to rule them all
-              exa               # alias ls
-              fd                # alias find
-              fish              # thanks for all the fish
-              fzf               # ctrl+r history
-              gettext           # translations
-              git-lfs           # large binary files in git
-              kubernetes-helm   # deploy things
-              jq                # query json
-              mill              # build stuff
-              niv               # manage nix dependencies
-              nixfmt            # fmt nix sources
-              nodejs            # whatnot
-              nox               # quick installer for nix
-              ripgrep-all       # rg faster grep on many file types
-              tig               # terminal git ui
-              victor-mono       # fontz ligatures
-            ];
+    home.packages = with pkgs; [
+      bat               # better cat
+      browsh            # Firefox on shell
+      coursier          # scala dependencies
+      dbmate            # migrations
+      direnv            # environment per project
+      emacsMacport      # one editor to rule them all
+      exa               # alias ls
+      fd                # alias find
+      fish              # thanks for all the fish
+      fzf               # ctrl+r history
+      gettext           # translations
+      git-lfs           # large binary files in git
+      kubernetes-helm   # deploy things
+      jq                # query json
+      mill              # build stuff
+      niv               # manage nix dependencies
+      nixfmt            # fmt nix sources
+      nodejs            # whatnot
+      nox               # quick installer for nix
+      ripgrep-all       # rg faster grep on many file types
+      tig               # terminal git ui
+      victor-mono       # fontz ligatures
+    ];
+
+    programs.git = {
+      enable = true;
+      userName = "Victor Borja";
+      userEmail = "vborja@apache.org";
+    };
+
   };
+
+
 }
