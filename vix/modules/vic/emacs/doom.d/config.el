ba2e476861b55cba371c4e0e78f834d4f2c4a63d
Victor Hugo Borja <vborja@apache.org>
2021-07-19 19:06:10 -0500

    Prefer SPC SPC to M-x. and <normal> U for undo.

--- vix/modules/vic/emacs/doom.d/config.el
+++ vix/modules/vic/emacs/doom.d/config.el
@@ -53,3 +53,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(map! :n "U" #'evil-redo)
+
+(map! :leader
+      :desc "M-x" "SPC" #'execute-extended-command)
