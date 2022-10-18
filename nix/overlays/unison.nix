b288c6d6c87a282e00e91cc52e8efed138073ca2
Victor Hugo Borja <vborja@apache.org>
2022-10-18 01:07:34 -0500

    nixfmt

--- nix/overlays/unison.nix
+++ nix/overlays/unison.nix
@@ -1,16 +1,16 @@
-nivSources:
-self: super: {
+nivSources: self: super: {
   unison-ucm = let
     inherit (self.stdenvNoCC) mkDerivation;
-  in mkDerivation {
-    name = "unison-ucm";
-    src = nivSources.ucm;
-    phases = [ "install" ];
-    buildInputs = with self; [ coreutils ];
-    install = ''
-    mkdir -p $out/bin
-    tar -xvf $src -C $out
-    ln -s $out/ucm $out/bin/ucm
-    '';
-  };
+  in
+    mkDerivation {
+      name = "unison-ucm";
+      src = nivSources.ucm;
+      phases = ["install"];
+      buildInputs = with self; [coreutils];
+      install = ''
+        mkdir -p $out/bin
+        tar -xvf $src -C $out
+        ln -s $out/ucm $out/bin/ucm
+      '';
+    };
 }
