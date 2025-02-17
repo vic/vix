1433de31ad5f862608202a680e7c8da69944c2b4
Victor Borja <vborja@apache.org>
2025-02-17 21:51:43 +0000

    rename files

--- modules/home/vic/secrets.nix
+++ modules/home/vic/secrets.nix
@@ -22,7 +22,7 @@
     "hello" = { };
     "ssh/id_ed25519" = {
       format = "binary";
-      sopsFile = ./secrets/ssh/id_ed25519;
+      sopsFile = ./secrets/ssh/vic_mordor;
     };
   };
 
@@ -32,7 +32,7 @@
     '';
   };
 
-  home.file.".ssh/id_ed25519.pub".source = ./secrets/ssh/id_ed25519.pub;
+  home.file.".ssh/id_ed25519.pub".source = ./secrets/ssh/vic_mordor.pub;
   home.activation.link_ssh_id = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     run ln -sf "${config.sops.secrets."ssh/id_ed25519".path}" $HOME/.ssh/id_ed25519
   '';
