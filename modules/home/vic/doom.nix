ca195fa6289015fcd2681330669302f7ddb1602a
Victor Borja <vborja@apache.org>
2025-02-18 21:49:28 -0600

    Dont link doom files

--- modules/home/vic/doom.nix
+++ modules/home/vic/doom.nix
@@ -3,7 +3,7 @@
   programs.emacs.enable = true;
   services.emacs.enable = true;
 
-  home.file.".config/doom".source = ./doom;
+  # home.file.".config/doom".source = ./doom;
   home.packages = [
     (pkgs.writeShellScriptBin "doom" ''exec $HOME/.config/emacs/bin/doom "$@"'')
     (pkgs.writeShellScriptBin "doomscript" ''exec $HOME/.config/emacs/bin/doomscript "$@"'')
