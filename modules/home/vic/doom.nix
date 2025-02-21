efdea895a6922874aa5e02bb5f34dca12cdae352
Victor Borja <vborja@apache.org>
2025-02-21 14:36:41 -0600

    softlink doom config

--- modules/home/vic/doom.nix
+++ modules/home/vic/doom.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.emacs.enable = true;
   services.emacs.enable = pkgs.stdenv.isLinux;
@@ -9,5 +9,9 @@
     (pkgs.writeShellScriptBin "doomscript" ''exec $HOME/.config/emacs/bin/doomscript "$@"'')
     (pkgs.writeShellScriptBin "d" ''exec emacsclient -nw -a "doom run -nw --"  "$@"'')
   ];
+
+  home.activation.link-doom-config = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  run ln -sfn $HOME/.flake/modules/home/vic/doom $HOME/.config/doom
+  '';
 
 }
