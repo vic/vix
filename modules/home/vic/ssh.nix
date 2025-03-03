8847e53404429f71d62d889931ecab3f5d2ea530
Victor Borja <vborja@apache.org>
2025-03-03 12:47:45 -0600

    Add yavanna ssh alias

--- modules/home/vic/ssh.nix
+++ modules/home/vic/ssh.nix
@@ -15,6 +15,12 @@
     #settings.KdbInteractiveAuthentication = false;
     #settings.PermitRootLogin = "no";
 
+    extraConfig = ''
+    Host yavanna
+      HostName 192.168.1.71
+      User vic
+      IdentityFile ~/.ssh/id_ed25519
+    '';
   };
 
   services.ssh-agent.enable = pkgs.stdenv.isLinux;
@@ -22,6 +28,8 @@
   home.file.".ssh/id_ed25519.pub".source = ./secrets/ssh/vic_mordor.pub;
 
   home.activation.link_ssh_id = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    run cat ${./secrets/ssh/vic_mordor.pub} > $HOME/.ssh/authorized_keys
+    run chmod 400 $HOME/.ssh/authorized_keys
     run ln -sf "${config.sops.secrets."ssh/id_ed25519".path}" $HOME/.ssh/id_ed25519
   '';
 }
