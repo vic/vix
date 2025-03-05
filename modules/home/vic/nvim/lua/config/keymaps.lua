a8e8f3c0bc0819a5bf4ea87f5edb31c3c9d14da7
Victor Borja <vborja@apache.org>
2025-03-04 20:14:33 -0600

    Save file with <leader>fs on nvim

--- modules/home/vic/nvim/lua/config/keymaps.lua
+++ modules/home/vic/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+local map = vim.keymap.set
+
+map("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save File" })
