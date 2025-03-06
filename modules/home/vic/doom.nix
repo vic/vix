d723abe465521d7a80cc9a633ab9fe940bf08a22
Victor Borja <vborja@apache.org>
2025-03-06 08:20:22 -0600

    clone doom if not found

--- modules/home/vic/doom.nix
+++ modules/home/vic/doom.nix
@@ -1,4 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }: let
+
+  doom-clone = pkgs.writeShellApplication {
+    name = "doom-clone";
+    runtimeInputs = with pkgs; [ git emacs ];
+    text = ''
+    # If doom already exists just exit
+    test -f "$HOME/.config/emacs/bin/doom" && exit 0
+
+    git clone --depth 1 https://github.com/doomemacs/doomemacs "$HOME/.config/emacs"
+    "$HOME/.config/emacs/bin/doom" install --no-config --no-env --install --force
+    '';
+  };
+
+in
 {
   programs.emacs.enable = true;
   services.emacs.enable = pkgs.stdenv.isLinux;
@@ -8,5 +22,9 @@
     (pkgs.writeShellScriptBin "doomscript" ''exec $HOME/.config/emacs/bin/doomscript "$@"'')
     (pkgs.writeShellScriptBin "d" ''exec emacsclient -nw -a "doom run -nw --"  "$@"'')
   ];
+
+  home.activation.clone_doom = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    run ${lib.getExe doom-clone}
+  '';
 
 }
