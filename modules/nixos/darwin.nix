20a9923904adb95417cd2c619ea2a049e113a45b
Victor Borja <vborja@apache.org>
2025-03-07 01:02:45 -0600

    install terminals on darwin

--- modules/nixos/darwin.nix
+++ modules/nixos/darwin.nix
@@ -1,12 +1,20 @@
-{ perSystem, ... }:
-{
-
-  environment.systemPackages = with perSystem.nix-darwin; [
+{ perSystem, pkgs, ... }:
+let
+  nix-darwin-pkgs = with perSystem.nix-darwin; [
     darwin-option
     darwin-rebuild
     darwin-version
     darwin-uninstaller
   ];
+in
+{
+
+  environment.systemPackages =
+    nix-darwin-pkgs
+    ++ (with pkgs; [
+      iterm2
+      wezterm
+    ]);
 
   # Determinate uses its own daemon to manage the Nix installation
   nix.enable = false;
