924660f21d9a2346946ad8bc8289566b140ed821
Victor Borja <vborja@apache.org>
2025-03-08 18:29:35 -0600

    Use laserwave on doom

--- modules/home/vic/dots/config/doom/config.el
+++ modules/home/vic/dots/config/doom/config.el
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-laserwave)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
