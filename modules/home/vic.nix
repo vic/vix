5413740cfcdccb1e85bb10aabebbb2dd1a7ae860
Victor Borja <vborja@apache.org>
2025-03-11 08:13:25 -0600

    copy from vim

--- modules/home/vic.nix
+++ modules/home/vic.nix
@@ -18,24 +18,30 @@
     ./vic/dots.nix
   ];
 
-  home.packages = with pkgs; [
-    tree
-    perSystem.nox.default
-    perSystem.self.leader
-    jujutsu # git
-    fzf
-    ripgrep # grep
-    bat # cat
-    bottom
-    htop
-    yazi # file tui
-    eza # ls
-    zoxide # cd
-    obsidian # notion
-    fd # find
-    lazygit # no magit
-    tig # alucard
-  ];
+  home.packages =
+    (with pkgs; [
+      tree
+      perSystem.nox.default
+      perSystem.self.leader
+      jujutsu # git
+      fzf
+      ripgrep # grep
+      bat # cat
+      bottom
+      htop
+      yazi # file tui
+      eza # ls
+      zoxide # cd
+      obsidian # notion
+      fd # find
+      lazygit # no magit
+      tig # alucard
+      cachix
+
+    ])
+    ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [
+      pkgs.wl-clipboard
+    ]);
 
   programs.nh.enable = true;
   programs.home-manager.enable = true;
