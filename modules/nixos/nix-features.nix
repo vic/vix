39eb8b7ec31913dd2d26455108faae88c586f33c
Victor Borja <vborja@apache.org>
2025-03-26 16:45:03 -0600

    unfree by host

--- modules/nixos/nix-features.nix
+++ modules/nixos/nix-features.nix
@@ -4,13 +4,23 @@
     pkg:
     let
       name = pkgs.lib.getName pkg;
-      allowed = builtins.elem name [
+
+      global = [
         "copilot-language-server"
         "cursor"
         "vscode"
         "obsidian"
         "anydesk"
       ];
+
+      perHost = {
+        "mordor" = [
+          "nvidia-x11"
+          "nvidia-settings"
+        ];
+      }.${config.networking.hostName};
+
+      allowed = builtins.elem name (global ++ perHost);
       msg = if allowed then "Allowed unfree: ${name}" else "Not allowed unfree ${name}";
     in
     builtins.trace msg allowed;
