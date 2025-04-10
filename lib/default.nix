ada53cec8a49c8621316954fd325814400e9312e
Victor Borja <vborja@apache.org>
2025-04-09 19:51:05 -0600

    Build each system on its own job

--- lib/default.nix
+++ lib/default.nix
@@ -3,4 +3,20 @@
 
   treefmt = pkgs: pkgs.callPackage ./treefmt.nix { inherit inputs; };
 
+  hostsBySystem =
+    system:
+    let
+      # pkgs expects we run on a linux.
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      lib = pkgs.lib;
+      where =
+        ({
+          "x86_64-linux" = inputs.self.nixosConfigurations;
+          "x86_64-darwin" = inputs.self.darwinConfigurations;
+          "aarch64-darwin" = inputs.self.darwinConfigurations;
+        }).${system} or (throw "Unsupported system: ${system}");
+      sameSystem = lib.filterAttrs (_: v: v.config.nixpkgs.hostPlatform.system == system) where;
+    in
+    lib.attrNames sameSystem;
+
 }
