0b2f2b89c9c21c0dd1de2636f190d1795e3d28d8
Victor Borja <vborja@apache.org>
2025-04-10 01:19:59 -0600

    try in foreground

--- devshells/upterm/gh-action.bash
+++ devshells/upterm/gh-action.bash
@@ -3,14 +3,15 @@ touch ~/.upterm/out.log ~/.upterm/upterm.log
 ssh-keygen -q -t rsa -N '' -C "$GITHUB_TRIGGERING_ACTOR@$GITHUB_REPOSITORY:$GITHUB_RUN_ID" -f ~/.ssh/id_rsa
 ssh-keygen -q -t ed25519 -N '' -C "$GITHUB_TRIGGERING_ACTOR@$GITHUB_REPOSITORY:$GITHUB_RUN_ID" -f ~/.ssh/id_ed25519
 ssh-add
-# ssh-keyscan uptermd.upterm.dev 2> /dev/null >> ~/.ssh/known_hosts
-# cat <(cat ~/.ssh/known_hosts | awk '{ print "@cert-authority * " $2 " " $3 }') >> ~/.ssh/known_hosts
 echo Adding uptermd.upterm.dev to known_hosts
-ssh-keyscan uptermd.upterm.dev | grep -v '#' \
-  | awk '{ print "@cert-authority " $1 " " $2 " " $3 }' \
-  | tee -a ~/.ssh/known_hosts
+ssh-keyscan uptermd.upterm.dev 2> /dev/null >> ~/.ssh/known_hosts2
+cat ~/.ssh/known_hosts2 >> ~/.ssh/known_hosts
+awk -f '{ print "@cert-authority * " $2 " " $3 }' -- ~/.ssh/known_hosts2 >> ~/.ssh/known_hosts
+cat ~/.ssh/known_hosts
 echo Starting in background
-screen -T xterm -dmS upterm bash -c "upterm host --accept --github-user vic 2>&1 | tee -a ~/.upterm/out.log"
+set -x -e
+upterm host --accept --github-user $GITHUB_REPOSITORY_OWNER  2>&1 | tee -a ~/.upterm/out.log
+#screen -T xterm -dmS upterm bash -c "upterm host --accept --github-user vic 2>&1 | tee -a ~/.upterm/out.log"
 sleep 1
 echo Waiting for upterm to start
 tail -f ~/.upterm/out.log | head -n 11
