5e0e33afb05824c7e58382373b11823693d59078
Victor Borja <vborja@apache.org>
2025-04-10 00:02:01 -0600

    Also log stderr

--- devshells/upterm/gh-action.bash
+++ devshells/upterm/gh-action.bash
@@ -1,6 +1,5 @@
 mkdir -p ~/.ssh ~/.upterm
 touch ~/.upterm/out.log ~/.upterm/upterm.log
-cat ~/.ssh/config
 ssh-keygen -q -t rsa -N '' -C "$GITHUB_TRIGGERING_ACTOR@$GITHUB_REPOSITORY:$GITHUB_RUN_ID" -f ~/.ssh/id_rsa
 ssh-keygen -q -t ed25519 -N '' -C "$GITHUB_TRIGGERING_ACTOR@$GITHUB_REPOSITORY:$GITHUB_RUN_ID" -f ~/.ssh/id_ed25519
 ssh-add
@@ -11,10 +10,12 @@ ssh-keyscan uptermd.upterm.dev | grep -v '#' \
   | awk '{ print "@cert-authority " $1 " " $2 " " $3 }' \
   | tee -a ~/.ssh/known_hosts
 echo Starting in background
-screen -A -T xterm -U -O -dmS upterm bash -c "upterm host --accept --github-user vic | tee -a ~/.upterm/out.log"
+screen -T xterm -dmS upterm bash -c "upterm host --accept --github-user vic 2>&1 | tee -a ~/.upterm/out.log"
+sleep 1
+echo Waiting for upterm to start
 tail -f ~/.upterm/out.log | head -n 11
 while ! grep 'SSH Session:' ~/.upterm/out.log; do sleep 1 ; done
-echo Waiting
+echo Waiting for join
 while ! grep 'Client joined' ~/.upterm/upterm.log >/dev/null; do sleep 10; done
 echo Joined
 while ! grep 'Client left' ~/.upterm/upterm.log >/dev/null; do sleep 10; done
