00957fcd2befac61d9a93140e122f5d54cc88377
Victor Borja <vborja@apache.org>
2025-04-10 01:55:13 -0600

    prefix path

--- devshells/upterm.nix
+++ devshells/upterm.nix
@@ -15,20 +15,22 @@ let
       UpdateHostKeys yes
   '';
 
+  runtimeInputs = [
+    pkgs.upterm
+    pkgs.screen
+    pkgs.openssh
+    pkgs.bash
+    pkgs.coreutils
+    pkgs.findutils
+    pkgs.gawk
+  ] ++ (pkgs.lib.optionals (pkgs.config.allowUnfree) [ pkgs.vscode ]);
+
   gh-action = pkgs.writeShellApplication {
     name = "gh-action";
-    runtimeInputs = [
-      pkgs.upterm
-      pkgs.screen
-      pkgs.openssh
-      pkgs.bash
-      pkgs.coreutils
-      pkgs.findutils
-      pkgs.gawk
-    ] ++ (pkgs.lib.optionals (pkgs.config.allowUnfree) [ pkgs.vscode ]);
     text = ''
       mkdir -p ~/.ssh
       echo "${ssh_config_text}" > ~/.ssh/config
+      export PATH="${pkgs.lib.makeBinPath runtimeInputs}:$PATH"
       ${pkgs.openssh}/bin/ssh-agent ${pkgs.bash}/bin/bash ${./upterm/gh-action.bash}
     '';
   };
