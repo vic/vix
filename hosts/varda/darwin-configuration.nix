3d40e0dce70a841ca48cea1d11fa5d01c172a5b5
Victor Borja <vborja@apache.org>
2025-04-09 20:02:28 -0600

    add varda

---
+++ hosts/varda/darwin-configuration.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }:
+{
+  imports = with inputs.self.nixosModules; [
+    darwin
+    nix-features
+    vic
+    ./static.nix
+  ];
+
+  users.users.vic.home = "/Users/vic";
+}
---
+++ hosts/varda/static.nix
@@ -0,0 +1,5 @@
+{
+  networking.hostName = "varda";
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  system.stateVersion = 6;
+}
