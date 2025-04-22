2678dc5dbe259660810780d8001f1aea4d974fbb
Victor Borja <vborja@apache.org>
2025-04-22 12:13:41 -0600

    enable powerManagement

--- modules/nixos/bootable.nix
+++ modules/nixos/bootable.nix
@@ -7,6 +7,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  powerManagement.enable = true;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
