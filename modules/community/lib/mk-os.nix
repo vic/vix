7ae563d53253eb0d34480930d7db59248e520eed
Victor Borja <vborja@apache.org>
2025-06-22 13:52:54 -0600

    community: Share lib.mk-os to instantiate nixos and nix-darwin

---
+++ modules/community/lib/mk-os.nix
@@ -0,0 +1,51 @@
+{ inputs, lib, ... }:
+let
+  flake.lib.mk-os = {
+    inherit mkNixos mkDarwin;
+    inherit wsl linux linux-arm;
+    inherit darwin darwin-intel;
+  };
+
+  wsl = mkNixos "x86_64-linux" "wsl";
+
+  linux = mkNixos "x86_64-linux" "nixos";
+  linux-arm = mkNixos "aarch64-linux" "nixos";
+
+  darwin-intel = mkDarwin "x86_64-darwin";
+  darwin = mkDarwin "aarch64-darwin";
+
+  mkNixos =
+    system: cls: name:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        inputs.self.modules.nixos.${cls}
+        inputs.self.modules.nixos.${name}
+        inputs.self.modules.nixos.${system}
+        {
+          networking.hostName = lib.mkDefault name;
+          nixpkgs.hostPlatform = lib.mkDefault system;
+          system.stateVersion = "25.05";
+        }
+      ];
+    };
+
+  mkDarwin =
+    system: name:
+    inputs.nix-darwin.lib.darwinSystem {
+      inherit system;
+      modules = [
+        inputs.self.modules.darwin.darwin
+        inputs.self.modules.darwin.${name}
+        inputs.self.modules.darwin.${system}
+        {
+          networking.hostName = lib.mkDefault name;
+          nixpkgs.hostPlatform = lib.mkDefault system;
+          system.stateVersion = 6;
+        }
+      ];
+    };
+in
+{
+  inherit flake;
+}
