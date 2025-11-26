0d48594346ba195af3aef2fd97e27338ac608acb
Victor Borja <vborja@apache.org>
2025-11-26 10:59:01 -0600

    rotates

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -2,7 +2,9 @@
 {
   flake-file.inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
   flake-file.inputs.flake-file.url = lib.mkDefault "github:vic/flake-file";
+  flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
   imports = [
-    inputs.flake-file.flakeModules.dendritic
+    (inputs.flake-file.flakeModules.dendritic or { })
+    (inputs.den.flakeModules.dendritic or { })
   ];
 }
